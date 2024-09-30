2005a208afb6457083b0509285b5e8dca42f15fc
MikeHE-CODER <a1918791@adelaide.edu.au>
2024-09-30 10:12:07 +0930

    Create UnitTest.h

---
+++ UnitTest.h
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "Addition.h"
+
+class UnitTest
+{
+public:
+    void runTests()
+    {
+        testAddition();
+    }
+
+private:
+    void testAddition()
+    {
+        Addition addition;
+
+        if (addition.add(1, 2) != 3)
+        {
+            std::cout << "Test 1 failed!" << std::endl;
+        }
+        // Add more tests here
+        if (addition.add(2, 2) != 4)
+        {
+            std::cout << "Test 2 failed!" << std::endl;
+        }
+        if (addition.add(3, 2) != 5)
+        {
+            std::cout << "Test 3 failed!" << std::endl;
+        }
+    }
+};
